bebbcc42c52e04e26426a4f9fc881791cf4093d3
Virag Kormoczy <vkormoczy@gmail.com>
2022-11-30 12:44:00 +0200

    Add jquery functionality

---
+++ web-safe-font-preview/main.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+  $('#text').on('keyup', (event) => {
+    $('.preview').html($(event.currentTarget).val());
+  });
+
+  $('#font').on('change', (event) => {
+    $('.preview').css('font-family', $(event.currentTarget).val());
+  });
+
+  $('#weight').on('change', (event) => {
+    $('.preview').css('font-weight', $(event.currentTarget).val());
+  });
+
+  $('#size').on('keyup', (event) => {
+    const fontSize = $(event.currentTarget).val() + 'px';
+    $('.preview').css('font-size', fontSize);
+  });
+});
